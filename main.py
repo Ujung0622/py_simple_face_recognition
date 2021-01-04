867f5747a5d185e6e4f9d29f8021171cc0d04e90
Ujung0622 <Ujung0622@users.noreply.github.com>
2021-01-04 22:09:04 +0900

    임포트완료

---
+++ main.py
@@ -0,0 +1,9 @@
+import dlib, cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.patheffects as path_effects
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('models/dlib_face_regonition_resnet_model_v1.dat')
